2057a8057ec8a1ade03fecf3a88cb13232cc4745
Maria Clara <98410548+mclara8@users.noreply.github.com>
2024-09-13 22:17:54 -0300

    funções com retorno e sem retorno

--- algoritmos/funcoes.c
+++ algoritmos/funcoes.c
@@ -1,24 +1,21 @@
-// esqueleto de um procedimento em C (não retorna um valor);
-
 #include <stdio.h>
 
+// função sem retorno
 void saudacao() {
     printf("Olá mundo!");
 }
 
-int main() {
-    saudacao(); // chamada do procedimento
-    return 0; 
-}
-
-// esqueleto de uma função em C (retorna um valor);      
-
+// função com retorno
 int soma(int num1, int num2) {
     return num1 + num2
 }
 
 int main() {
+
+    saudacao(); // chamada do procedimento 
+
     int resultado = soma(10, 11); // chamando função e armazenando resultado
     printf("O resultado da soma é: %d\n", resultado);
+    
     return 0;
 }
